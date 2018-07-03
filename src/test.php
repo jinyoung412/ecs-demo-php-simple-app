261c1a4dfe3f02ab60f43d4190e833331460036f
jinyoung412 <40601943+jinyoung412@users.noreply.github.com>
2018-07-03 11:19:11 -0400

    Add files via upload

---
+++ src/test.php
@@ -0,0 +1,70 @@
+<?php
+ob_start();
+if(isset($_POST['submit'])){
+    
+   // echo json_encode($_POST,JSON_PRETTY_PRINT);
+ //   die();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "deliveryorder";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    header("location:registration.php?success=false");
+
+die("Connection failed: " . $conn->connect_error);
+}
+    
+echo "Connected Successfully";
+
+/*$sql = "INSERT INTO order (full_name, address1, city)
+VALUES (?,?,?,?,?,?,?)";
+    
+$stmt = mysqli_prepare($conn, $sql);
+$stmt->bind_param("sss", $_POST['stu_name'], $_POST['address1'], $_POST['city'],$_POST['state'],$_POST['zip'],$_POST['country'],$_POST['phonenum']);
+
+$stmt->execute();
+
+if ($conn->query($sql) === TRUE) {
+echo "<script type= 'text/javascript'>alert('New record created successfully');</script>";
+} else {
+echo "<script type= 'text/javascript'>alert('Error: " . $sql . "<br>" . $conn->error."');</script>";
+}
+
+$conn->close();*/
+    
+
+    $name = $_POST['stu_name'];
+    $address = $_POST['address1'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $country = $_POST['country'];
+    $phonenum = $_POST['phonenum'];
+    $participate = isset($_POST['participate'])?'true':'false';
+    $item = isset($_POST['item'])?'true':'false';
+    $itemlist = $_POST['itemlist'];
+
+
+
+    $sql = "INSERT INTO registration (name,address,city,state,zip,country,phonenum,participate,item,itemlist)
+    VALUES ('$name', '$address', '$city','$state', '$zip', '$country', '$phonenum','$participate','$item','$itemlist')";
+
+
+    if ($conn->query($sql) === TRUE) {
+    echo "<script type= 'text/javascript'>alert('New record created successfully');</script>";
+    } else {
+    echo "<script type= 'text/javascript'>alert('Error: " . $sql . "<br>" . $conn->error."');</script>";
+    }
+    
+    /*header("Location:index.html?signup=success");*/    
+    
+    $conn->close();
+}
+echo ob_get_clean();
+
+header("location:registration.php?success=true");
+?>
