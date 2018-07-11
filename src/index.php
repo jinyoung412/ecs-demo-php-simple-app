eabf025aec4e644c713f7ae5fd75ea07150e2edc
jinyoung412 <40601943+jinyoung412@users.noreply.github.com>
2018-07-11 12:44:17 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -92,7 +92,7 @@
 <!--<link rel="stylesheet" type="text/css" href="style.css">-->
     
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="css/style.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
